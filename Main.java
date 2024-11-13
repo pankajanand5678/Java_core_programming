6266cc92b0f53ba711e898ef06b3f86899dd0b09
anand <pankajanand5678@gmail.com>
2024-11-13 15:20:59 +0530

    ignore file updated

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String []args){
-        System.out.println("Hello to g  it and github...");
+        System.out.println("Hello to git and github...");
     } 
 }
